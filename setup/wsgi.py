eaf24f3747556fe0247fde5381ac29ef84620792
Juan_de_sousa_cardoso <juandesouzacardoso@outlook.com>
2025-02-14 05:31:34 -0300

    musei settings pra setup

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -15,7 +15,7 @@ from django.core.wsgi import get_wsgi_application
 #acrescentei isso
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'setup.development'
 #acrescentei isso 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
