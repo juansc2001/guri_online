b21169a7b65667e8f97a1b4aa53ac6f72cc79d1d
Juan_de_sousa_cardoso <juandesouzacardoso@outlook.com>
2025-02-14 13:32:23 -0300

    recoloquei o staticfiles_DIRS

--- setup/settings.py
+++ setup/settings.py
@@ -125,10 +125,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #acrescentado
 
-'''
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',   REMOVIDO
-]'''
+    BASE_DIR / 'static',   #REcoloquei
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
