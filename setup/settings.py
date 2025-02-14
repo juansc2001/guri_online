a5d30d3f39e90db573357c15ece253f23257ef53
Juan_de_sousa_cardoso <juandesouzacardoso@outlook.com>
2025-02-14 05:05:47 -0300

    Revert "criado pasta setup"
    This reverts commit a2b65a0c52b91780031a3dd23043f66bdd3a3437.

--- setup/settings.py
+++ setup/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
