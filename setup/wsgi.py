511adbd1732b31574585b68401f8dae03f57807a
Juan_de_sousa_cardoso <juandesouzacardoso@outlook.com>
2025-02-14 05:22:42 -0300

    importei sys e mechi wsgi

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+#acrescentei isso
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
+#acrescentei isso 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
