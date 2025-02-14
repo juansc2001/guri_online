45776e9c8db7270c0caf50cea2880bafd199fadb
Juan_de_sousa_cardoso <juandesouzacardoso@outlook.com>
2025-02-14 11:31:21 -0300

    #

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -13,11 +13,11 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 #acrescentei isso
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(BASE_DIR)
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
 #acrescentei isso 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.wsgi')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
 application = get_wsgi_application()
