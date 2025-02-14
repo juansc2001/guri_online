209feefd349b7fa822f5aa2c8854fbb017ed16af
PROGRAMAÇÃO EM PYTHON 2024.4 <51129532024.4@senacrjedu.local>
2025-02-14 19:31:11 -0300

    #

--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", #adicionado
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+'''
 STATIC_URL = 'static/'
 
 #acrescentado
@@ -133,6 +135,15 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+'''
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Se você tiver uma pasta 'static' no projeto
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Onde o Whitenoise vai coletar os arquivos
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
