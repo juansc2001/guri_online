c7e487a7e1a50bbd3a9fb721445867468a43f8ed
Juan_de_sousa_cardoso <juandesouzacardoso@outlook.com>
2025-02-14 11:38:34 -0300

    importei sys.path

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -8,16 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-#acrescentei isso
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#sys.path.append(BASE_DIR)
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.development'
-#acrescentei isso 
+# Adicionando o diretório base ao sys.path (caso necessário)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
 
+# Definindo a variável de ambiente do Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
 application = get_wsgi_application()
