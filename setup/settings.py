7b5ede578ca8f056026c99fd9b502c4c5accd446
PROGRAMAÇÃO EM PYTHON 2024.4 <51129532024.4@senacrjedu.local>
2025-02-14 21:16:35 -0300

    #

--- setup/settings.py
+++ setup/settings.py
@@ -151,7 +151,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
 #if not DEBUG:
-#   STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
